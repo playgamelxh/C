8c49c5905f7123b7632ff377fca2536756e52fe7
lxh <lxh@lxhdeMacBook-Pro.local>
2017-09-30 14:25:52 +0800

    添加修改

---
+++ 1/static.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+static int j;
+
+void fun1(void)
+{
+	static int i = 0;
+	i++;
+}
+
+void fun2(void)
+{
+	j = 0;
+	j++;
+}
+
+int main()
+{
+	for(int k=0;k<10;k++)
+	{
+		fun1();
+		fun2();
+	}
+	return 0;
+}
